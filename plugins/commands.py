7d414ccb5619d8abc0318d780774dd10d3753050
Vansh Kumar <145826508+vansh5004@users.noreply.github.com>
2024-01-17 18:41:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1008,7 +1008,7 @@ def send_telegram_message(chat_id, message):
     updater.bot.send_message(chat_id=chat_id, text=message)
 
 @Client.on_message(filters.command("myplan"))
-async def myplan(bot, message):
+async def myplan(client,message):
     user_id = str(update.message.from_user.id)
     if user_id in PREMIUM_USER:
         message = f"ʜᴇʏ {user_id},\n\nYᴏᴜ Hᴀᴠᴇ Aᴄᴛɪᴠᴇ Pʀᴇᴍɪᴜᴍ Pʟᴀɴ. Eɴɪᴏʏ Uɴʟɪᴍɪᴛᴇᴅ Mᴏᴠɪᴇ Wɪᴛʜᴏᴜᴛ Aᴅs."
@@ -1016,7 +1016,8 @@ async def myplan(bot, message):
   #      sleep(1)  # Sleep for 1 second to avoid rate limiting
     else:
         return
-    await message.reply_text("Sorry, you are not a premium user. Upgrade to premium for exclusive content!")
+    new_message = "Sorry, you are not a premium user. Upgrade to premium for exclusive content!"
+    await message.reply_text(new_message)
     
 
 
